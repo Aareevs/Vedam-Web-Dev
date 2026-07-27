0fa46e88af8ee0a533e8be94257cabd994eeb789
Aareevs <aareevs@gmail.com>
2026-07-27 13:05:50 +0530

    Added more React Components L1

--- Vedam Semester 3/React Sem 3/Lecture 1/src/App.jsx
+++ Vedam Semester 3/React Sem 3/Lecture 1/src/App.jsx
@@ -3,9 +3,11 @@ import { useState } from 'react';
 
 const App = () => {
   let [count, setCount] = useState(0);
+  let [color, setColor] = useState("red");
+  let [height, setHeight] = useState("100vh");
   return (
 
-    <div>
+    <div style={{backgroundColor:color, height:height}}>
       <h2>{count}</h2>
       <button onClick={()=>setCount(count+1)}>Increment</button>
       <button onClick={()=>setCount(0)}>Reset</button>
